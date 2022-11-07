15e64113c15309e32e92065efbfd436a7e964112
Amandus Butzer <amandus.butzer@heigit.org>
2022-11-07 11:51:13 +0100

    build: Add coverage for dev_app
    - uses cypress-vite with vite-plugin-istanbul to instrument
    the dev_app code and measure coverage of the app and src files through the
    e2e example.cy.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    istanbul({
+      include: ['src/*', 'dev_app/*'],
+      cypress: true,
+      exclude: ['node_modules', '**/__test__/**'],
+      extension: [ '.js', '.ts', '.vue' ],
+      requireEnv: true
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
