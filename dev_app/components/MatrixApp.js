77a8abfd2de167c864907aa6d08090b84132b5b7
Emma Hegarty <emma.hegarty@heigit.org>
2023-01-10 16:21:46 +0100

    feat: add Vue App files for OrsMatrix

---
+++ dev_app/components/MatrixApp.js
@@ -0,0 +1,41 @@
+import Openrouteservice from '@/index.js'
+
+export default {
+  props: {
+    msg: {
+      type: String,
+      required: false
+    }
+  },
+  data() {
+    return {
+      json_title: '',
+      json_data: {},
+      data_ready: false
+    }
+  },
+  methods: {},
+
+  async mounted() {
+    const orsMatrix = new Openrouteservice.Matrix({
+      api_key: import.meta.env.VITE_ORS_API_KEY
+    })
+
+    let response = {}
+    try {
+      response = await orsMatrix.calculate({
+        locations: [[8.690958, 49.404662], [8.687868, 49.390139], [8.687868, 49.390133]],
+        profile: "driving-car",
+        sources: ['all'],
+        destinations: ['all']
+      })
+      this.json_title = 'Response'
+      this.json_data = JSON.stringify(response, null, "\t")
+    } catch (e) {
+      this.json_title = 'Error'
+      this.json_data = JSON.stringify(e, null, "\t")
+    }
+
+    this.data_ready = true
+  }
+}
