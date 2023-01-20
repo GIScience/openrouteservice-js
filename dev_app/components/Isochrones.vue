d7bb274eb85570f2c5fd406dd7ca865a8c1254c4
Emma Hegarty <emma.hegarty@heigit.org>
2023-01-20 15:07:19 +0100

    feat: add Isochrones map

--- dev_app/components/Isochrones.vue
+++ dev_app/components/Isochrones.vue
@@ -3,10 +3,48 @@
 <template>
   <div>
     <h1 class='green'>{{ msg }}</h1>
-    <h3>
-
-    </h3>
   </div>
+
+  <div class='leaflet' v-if='map_ready'>
+    <l-map ref='map' :useGlobalLeaflet='false' v-model:zoom='zoom' :center='[center.lat, center.lon]'>
+      <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+      ></l-tile-layer>
+
+      <l-marker :lat-lng='[points[0][1], points[0][0]]'>
+        <l-icon :icon-url="blueIconUrl" :icon-size="iconSize" :icon-anchor="iconAnchor"> </l-icon>
+      </l-marker>
+      <l-geo-json :geojson='iso[0][5]' :options='hueStyle[5]'> </l-geo-json>
+      <l-geo-json :geojson='iso[0][4]' :options='hueStyle[4]'> </l-geo-json>
+      <l-geo-json :geojson='iso[0][3]' :options='hueStyle[3]'> </l-geo-json>
+      <l-geo-json :geojson='iso[0][2]' :options='hueStyle[2]'> </l-geo-json>
+      <l-geo-json :geojson='iso[0][1]' :options='hueStyle[1]'> </l-geo-json>
+      <l-geo-json :geojson='iso[0][0]' :options='hueStyle[0]'> </l-geo-json>
+
+      <l-marker :lat-lng='[points[1][1], points[1][0]]'>
+        <l-icon :icon-url="blueIconUrl" :icon-size="iconSize" :icon-anchor="iconAnchor"> </l-icon>
+      </l-marker>
+      <l-geo-json :geojson='iso[1][5]' :options='hueStyle[5]'> </l-geo-json>
+      <l-geo-json :geojson='iso[1][4]' :options='hueStyle[4]'> </l-geo-json>
+      <l-geo-json :geojson='iso[1][3]' :options='hueStyle[3]'> </l-geo-json>
+      <l-geo-json :geojson='iso[1][2]' :options='hueStyle[2]'> </l-geo-json>
+      <l-geo-json :geojson='iso[1][1]' :options='hueStyle[1]'> </l-geo-json>
+      <l-geo-json :geojson='iso[1][0]' :options='hueStyle[0]'> </l-geo-json>
+
+      <div v-if='hue_style'>
+        <l-marker :lat-lng='[points[0][1], points[0][0]]'>
+          <l-icon :icon-url="blueIconUrl" :icon-size="iconSize" :icon-anchor="iconAnchor"></l-icon>
+        </l-marker>
+        <div v-for='(hue, index) in hueStyle' :key='hue'>
+          <l-geo-json :geojson='iso[0][iso[0].length - index]' :options='hueStyle[index]'></l-geo-json>
+        </div>
+      </div>
+
+    </l-map>
+  </div>
+
   <div>
     <p class='ors_title' v-if='data_ready'>
       {{ json_title }}
@@ -20,6 +58,10 @@
 <style>
 .green {
   color: green;
+}
+.leaflet {
+  height: 540px;
+  width: 810px;
 }
 .ors_title {
   font-weight: bold;
--- dev_app/components/IsochronesApp.js
+++ dev_app/components/IsochronesApp.js
@@ -1,6 +1,20 @@
 import Openrouteservice from '@/index.js'
 
+import blueIcon from './icons/blue_icon.png'
+import redIcon from './icons/red_icon.png'
+import greenIcon from './icons/green_icon.png'
+
+import "leaflet/dist/leaflet.css";
+import {LMap, LTileLayer, LMarker, LIcon, LGeoJson} from "@vue-leaflet/vue-leaflet";
+
 export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LIcon,
+    LGeoJson
+  },
   props: {
     msg: {
       type: String,
@@ -9,9 +23,35 @@ export default {
   },
   data() {
     return {
+      zoom: 13,
+      points: [],
+      extent: 600,
+      isochrones: [],
+      center: {
+        lat: 0,
+        lon: 0
+      },
+      geojson: {},
+      hueStyle: [],
+      iso: [],
+
+
       json_title: '',
       json_data: {},
-      data_ready: false
+      data_ready: false,
+      map_ready: false
+
+    }
+  },
+  computed: {
+    blueIconUrl() {
+      return blueIcon
+    },
+    iconSize() {
+      return [25, 41]
+    },
+    iconAnchor() {
+      return [15, 41]
     }
   },
   methods: {},
@@ -21,15 +61,154 @@ export default {
       api_key: import.meta.env.VITE_ORS_API_KEY
     })
 
+    // add center point for isochrones here
+    this.points = [[8.681495, 49.41461]]
+
+    for (let i = 0; i < (this.extent / 100); i++) {
+      let z = i + 1
+      this.isochrones[i] = z * 100;
+    }
+
     let response = {}
     try {
       response = await orsIsochrones.calculate({
         profile: 'driving-car',
-        locations: [[8.681495, 49.41461], [8.686507, 49.41943]],
-        range: [300, 200],
-        area_units: 'km'
+        locations: this.points,
+        range: this.isochrones,
+        area_units: 'km',
+        format: 'geojson'
       })
       this.json_title = 'Response'
+      this.center = {
+        lat: (response.bbox[1] + response.bbox[3]) / 2,
+        lon: (response.bbox[0] + response.bbox[2]) / 2
+      }
+      // code to style isochrones
+      // divide response.features into the same amount of arrays as points requested, here: two
+      if (this.points.length <= 2) {
+        this.iso = [
+          response.features.slice(0, response.features.length / this.points.length),
+          response.features.slice(this.iso[0], response.features.length / this.points.length)
+        ]
+      }
+
+      // isochrones styled by changing opacity
+      if (this.points.length > 1) {
+        this.color_style = true
+      // isochrones styled by changing hue, push different hues to the hueStyle array
+      } else {
+        let i = 0
+        if (i < this.iso[0].length) {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(155, 100%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+          i++
+        }
+        if (i < this.iso[0].length) {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(95, 100%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+          i++
+        }
+        if (i < this.iso[0].length) {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(55, 100%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+          i++
+        }
+        if (i < this.iso[0].length) {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(25, 100%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+          i++
+        }
+        if (i < this.iso[0].length) {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(360, 100%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+          i++
+        }
+        if (i < this.iso[0].length) {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(315, 100%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+          i++
+        }
+        if (i < this.iso[0].length) {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(230, 100%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+          i++
+        }
+        if (i < this.iso[0].length) {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(240, 100%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+          i++
+        }
+        if (i < this.iso[0].length) {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(180, 100%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+        } else {
+          this.hueStyle.push({
+            style: {
+              color: 'hsl(0, 0%, 40%)',
+              opacity: 0.3,
+              fillOpacity: 0.2,
+              weight: 3
+            }
+          })
+        }
+        this.hue_style = true
+      }
+
+      this.geojson = response
       this.json_data = JSON.stringify(response, null, "\t")
     } catch (e) {
       this.json_title = 'Error'
@@ -37,5 +216,6 @@ export default {
     }
 
     this.data_ready = true
+    this.map_ready = true
   }
 }
