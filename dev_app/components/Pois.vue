f73dea0bff90e6d5e2ef33be07076485067dbb44
Emma Hegarty <emma.hegarty@heigit.org>
2023-01-24 16:02:41 +0100

    feat: add Pois map

--- dev_app/components/Pois.vue
+++ dev_app/components/Pois.vue
@@ -1,30 +1,51 @@
 <script src='./PoisApp.js'></script>
 
 <template>
-  <div>
-    <h1 class='green'>{{ msg }}</h1>
-    <h3>
+	<div>
+		<h1 class='green'>{{ msg }}</h1>
+	</div>
 
-    </h3>
-  </div>
-  <div>
-    <p class='ors_title' v-if='data_ready'>
-      {{ json_title }}
-    </p>
-    <p class='ors_call' v-if='data_ready'>
-      {{ json_data }}
-    </p>
-  </div>
+	<div class='leaflet' v-if='map_ready'>
+		<l-map ref='map' :useGlobalLeaflet='false' v-model:zoom='zoom' :center='[center.lat, center.lon]'>
+			<l-tile-layer
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				layer-type="base"
+				name="OpenStreetMap"
+			></l-tile-layer>
+
+			<l-marker :lat-lng='[coordinates[1], coordinates[0]]'>
+				<l-icon :icon-url="redIconUrl" :icon-size="iconSize" :icon-anchor="iconAnchor"></l-icon>
+			</l-marker>
+			<l-geo-json :geojson='geojson'></l-geo-json>
+
+		</l-map>
+	</div>
+
+	<div>
+		<p class='ors_title' v-if='data_ready'>
+			{{ json_title }}
+		</p>
+		<p class='ors_call' v-if='data_ready'>
+			{{ json_data }}
+		</p>
+	</div>
 </template>
 
 <style>
 .green {
-  color: green;
+	color: green;
 }
+
+.leaflet {
+	height: 540px;
+	width: 810px;
+}
+
 .ors_title {
-  font-weight: bold;
+	font-weight: bold;
 }
+
 .ors_call {
-  font-size: x-small;
+	font-size: x-small;
 }
 </style>
--- dev_app/components/PoisApp.js
+++ dev_app/components/PoisApp.js
@@ -1,6 +1,18 @@
 import Openrouteservice from '@/index.js'
 
+import redIcon from './icons/red_icon.png'
+
+import "leaflet/dist/leaflet.css";
+import {LMap, LTileLayer, LMarker, LIcon, LGeoJson} from "@vue-leaflet/vue-leaflet";
+
 export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LIcon,
+    LGeoJson
+  },
   props: {
     msg: {
       type: String,
@@ -9,9 +21,29 @@ export default {
   },
   data() {
     return {
+      zoom: 17,
+      coordinates: [],
+      center: {
+        lat: 0,
+        lon: 0
+      },
+      geojson: {},
+
       json_title: '',
       json_data: {},
-      data_ready: false
+      data_ready: false,
+      map_ready: false
+    }
+  },
+  computed: {
+    redIconUrl() {
+      return redIcon
+    },
+    iconSize() {
+      return [25, 41]
+    },
+    iconAnchor() {
+      return [15, 41]
     }
   },
   methods: {},
@@ -21,6 +53,9 @@ export default {
       api_key: import.meta.env.VITE_ORS_API_KEY
     })
 
+    // add coordinates of requested point here
+    this.coordinates = [8.8034, 53.0756]
+
     let response = {}
     try {
       response = await orsPois.pois({
@@ -28,13 +63,18 @@ export default {
           bbox: [[8.8034, 53.0756], [8.7834, 53.0456]],
           geojson: {
             type: "Point",
-            coordinates: [8.8034, 53.0756]
+            coordinates: this.coordinates
           },
           buffer: 250
         },
         timeout: 20000
       })
       this.json_title = 'Response'
+      this.center = {
+        lat: (response.bbox[1] + response.bbox[3]) / 2,
+        lon: (response.bbox[0] + response.bbox[2]) / 2
+      }
+      this.geojson = response
       this.json_data = JSON.stringify(response, null, "\t")
     } catch (e) {
       this.json_title = 'Error'
@@ -42,5 +82,6 @@ export default {
     }
 
     this.data_ready = true
+    this.map_ready = true
   }
 }
